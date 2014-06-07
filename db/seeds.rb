258bdcd76da83cc7628142dbdf7cb668d1e85518
Chris Boertien <chris.boertien@gmail.com>
2014-06-07 15:36:41 -0300

    show total resources raided
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,6 @@ Player.create!(name: 'Player').tap do |player|
     city.raids.create!(food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 2.days)
   end
   player.cities.create!(name: 'City 2', x: 5, y: 5).tap do |city|
+    city.raids.create!(food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 8.days)
   end
 end
