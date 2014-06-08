dd83c302b2cd89a73156373b1d0e5d3b425771ba
Chris Boertien <chris.boertien@gmail.com>
2014-06-07 23:39:41 -0300

    add some validations for raids and scouts
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/models/raid.rb
+++ app/models/raid.rb
@@ -2,6 +2,13 @@ class Raid < ActiveRecord::Base
   belongs_to :city
   belongs_to :player
 
+  validates :city,     presence: true
+  validates :player,   presence: true
+  validates :timber,   presence: true, numericality: {only_integer: true}
+  validates :bronze,   presence: true, numericality: {only_integer: true}
+  validates :food,     presence: true, numericality: {only_integer: true}
+  validates :capacity, presence: true, numericality: {only_integer: true}
+
   def self.last_week
     where('raided_at > ?', Time.now - 1.week)
   end
--- app/models/scout.rb
+++ app/models/scout.rb
@@ -2,6 +2,12 @@ class Scout < ActiveRecord::Base
   belongs_to :city
   belongs_to :player
 
+  validates :city,     presence: true
+  validates :player,   presence: true
+  validates :timber,   presence: true, numericality: {only_integer: true}
+  validates :bronze,   presence: true, numericality: {only_integer: true}
+  validates :food,     presence: true, numericality: {only_integer: true}
+
   def total_resources
     timber + bronze + food
   end
