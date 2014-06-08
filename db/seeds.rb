be9b06527492f87c7712bf1c7061e0ef337c1a72
Chris Boertien <chris.boertien@gmail.com>
2014-06-08 17:16:58 -0300

    set the player in raids in seeds
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@
 
 Player.create!(name: 'Player').tap do |player|
   player.cities.create!(name: 'City 1', x: 0, y: 0).tap do |city|
-    city.raids.create!(food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 2.days)
+    city.raids.create!(player: player, food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 2.days)
   end
   player.cities.create!(name: 'City 2', x: 5, y: 5).tap do |city|
-    city.raids.create!(food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 8.days)
+    city.raids.create!(player: player, food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 8.days)
   end
 end
