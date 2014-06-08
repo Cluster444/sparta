a9f6d40e185b17320797f59dd49ee881a65067a7
Chris Boertien <chris.boertien@gmail.com>
2014-06-08 17:26:55 -0300

    validate timestamps on battle reports
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/models/scout.rb
+++ app/models/scout.rb
@@ -2,9 +2,10 @@ class Scout < ActiveRecord::Base
   belongs_to :city
   belongs_to :player
 
-  validates :city,     presence: true
-  validates :player,   presence: true
-  validates :timber,   presence: true, numericality: {only_integer: true}
-  validates :bronze,   presence: true, numericality: {only_integer: true}
-  validates :food,     presence: true, numericality: {only_integer: true}
+  validates :city,       presence: true
+  validates :player,     presence: true
+  validates :timber,     presence: true, numericality: {only_integer: true}
+  validates :bronze,     presence: true, numericality: {only_integer: true}
+  validates :food,       presence: true, numericality: {only_integer: true}
+  validates :scouted_at, presence: true
 end
