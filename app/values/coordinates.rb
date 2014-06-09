c8f83a84683b46463e83bb7e3b7312e99dbc2544
Chris Boertien <chris.boertien@gmail.com>
2014-06-09 19:28:19 -0300

    test the distance function on coordinates
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/values/coordinates.rb
+++ app/values/coordinates.rb
@@ -1,4 +1,6 @@
 class Coordinates
+  attr_reader :x, :y
+
   def initialize(x,y)
     @x, @y = x, y
   end
---
+++ spec/values/coordinates_spec.rb
@@ -0,0 +1,9 @@
+require './app/values/coordinates'
+
+describe Coordinates do
+  it 'can tell the distance from another set of coordinates' do
+    a = Coordinates.new(0,0)
+    b = Coordinates.new(3,4)
+    expect(a.distance(b)).to eq 5.0
+  end
+end
