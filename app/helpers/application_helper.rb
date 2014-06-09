c9911091e96b8cbd6b4aec596dc5e217ac01a60b
Chris Boertien <chris.boertien@gmail.com>
2014-06-09 20:31:54 -0300

    setup a form wrapper and make coordinates settable for the player
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def form_wrapper(title=nil, options={}, &block)
+    options[:size] || 'medium'
+
+    columns, offset =
+      case options[:size]
+      when 'small'  then [4, 4]
+      when 'medium' then [6, 3]
+      when 'large'  then [8, 2]
+      end
+
+    render layout: 'shared/form_wrapper', locals: {title: title, column: columns, offset: offset}, &block
+  end
+
+  def column(size, options={}, &block)
+    options[:class] ||= ''
+    options[:class] ||= [options[:class], "col-md-#{size}"]
+    unless size == 12
+      options[:class] << "col-md-offset-#{(12 - size)/2}"
+    end
+    options[:class] = options[:class].join(' ').strip
+    content_tag(:div, options, &block)
+  end
 end
