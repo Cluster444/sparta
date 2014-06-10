4cfcfee630689f054f56e7b13b079ef269a0ec29
Chris Boertien <chris.boertien@gmail.com>
2014-06-10 00:30:22 -0300

    handle all forms with the form wrapper
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,25 @@
 module ApplicationHelper
+  def form_for(resource, options={}, &block)
+    options[:builder] ||= BootstrapFormBuilder
+    super(resource, options, &block)
+  end
+
   def form_wrapper(title=nil, options={}, &block)
-    options[:size] || 'medium'
+    options[:size] ||= 'medium'
 
     columns, offset =
       case options[:size]
-      when 'small'  then [4, 4]
-      when 'medium' then [6, 3]
-      when 'large'  then [8, 2]
+      when 'small'  then 4
+      when 'medium' then 6
+      when 'large'  then 8
       end
 
-    render layout: 'shared/form_wrapper', locals: {title: title, column: columns, offset: offset}, &block
+    render layout: 'shared/form_wrapper', locals: {title: title, columns: columns}, &block
   end
 
   def column(size, options={}, &block)
     options[:class] ||= ''
-    options[:class] ||= [options[:class], "col-md-#{size}"]
+    options[:class] = [options[:class], "col-md-#{size}"]
     unless size == 12
       options[:class] << "col-md-offset-#{(12 - size)/2}"
     end
