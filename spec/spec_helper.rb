7316ddc0a6b785e24badb22085c3d0e4710f0109
Chris Boertien <chris.boertien@gmail.com>
2014-06-11 14:34:46 -0300

    load active support and byebug in spec helper
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,6 @@
+require 'byebug'
+require 'active_support/all'
+
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
