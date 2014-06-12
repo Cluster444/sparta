c724c6934624833e054bf1447a97daae6ffac8f8
Chris Boertien <chris.boertien@gmail.com>
2014-06-12 12:06:18 -0300

    test for raid estimate
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -11,4 +11,22 @@ describe City do
     expect { city.update(level: 6.6) }.to change(LevelProgress, :count).by 1
     expect(LevelProgress.last.level).to eq 6.6
   end
+
+  describe 'raid estimate' do
+    it 'returns nil if the acropolis is not set' do
+      city = FactoryGirl.create(:city)
+      expect(city.estimated_raid_resources).to be_nil
+    end
+
+    it 'returns nil if the estimated resources is nil' do
+      city = FactoryGirl.create(:city, acropolis: 1000)
+      expect(city.estimated_raid_resources).to be_nil
+    end
+
+    it 'returns the total current resources minus the acropolis' do
+      city = FactoryGirl.create(:city, acropolis: 1000)
+      FactoryGirl.create(:scout, city: city, timber: 4000, bronze: 5000, food: 6000)
+      expect(city.estimated_raid_resources).to eq 12000
+    end
+  end
 end
