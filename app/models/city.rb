f6de8aaa64eaee06db0aa6a759ad286ea687592d
Chris Boertien <chris.boertien@gmail.com>
2014-06-12 11:52:42 -0300

    remove location and add raid estimate to cities table
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/models/city.rb
+++ app/models/city.rb
@@ -14,6 +14,17 @@ class City < ActiveRecord::Base
     ResourceEstimator.new(self, kind).estimate
   end
 
+  def estimated_raid_resources
+    return nil unless acropolis
+
+    t = current_resources(:timber)
+    b = current_resources(:bronze)
+    f = current_resources(:food)
+    return nil unless t && b && f
+
+    t + b + f - (acropolis * 3)
+  end
+
 private
 
   def track_level_progress
