cd15e28a620fb52f68c871c2abd1406ea36038a9
Chris Boertien <chris.boertien@gmail.com>
2014-06-16 18:04:00 -0300

    handle level progress in seeds
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-[City, Player, Raid, Scout].each &:delete_all
+[City, Player, Raid, Scout, LevelProgress].each &:delete_all
 
 Player.create!(name: 'Player').tap do |player|
-  player.cities.create!(name: 'City 1', x: 0, y: 0).tap do |city|
+  player.cities.create!(name: 'City 1', level: 5, x: 0, y: 0).tap do |city|
     city.raids.create!(player: player, food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 2.days)
   end
-  player.cities.create!(name: 'City 2', x: 5, y: 5).tap do |city|
+  player.cities.create!(name: 'City 2', level: 10, x: 5, y: 5).tap do |city|
     city.raids.create!(player: player, food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 8.days)
   end
 end
