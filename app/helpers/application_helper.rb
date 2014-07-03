3eeb667d7e79ee58a72d25c23e43bcd6fb00faaf
Chris Boertien <chris.boertien@gmail.com>
2014-07-03 15:01:48 -0300

    customize the time ago since last battle report
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -41,4 +41,18 @@ module ApplicationHelper
 
     render partial: 'shared/icon', locals: {name: name, key: key, title: options[:title]}
   end
+
+  def time_ago(time)
+    seconds_ago = Time.zone.now - time
+
+    if seconds_ago < 1.hour
+      pluralize(seconds_ago % 1.minute), 'minute')
+    elsif seconds_ago < 1.day
+      pluralize(seconds_ago % 1.hour), 'hour')
+    else
+      days_ago = seconds_ago % 1.day
+      hours_ago = (seconds_ago - days_ago * 1.day) % 1.hour
+      "#{pluralize(days_ago, 'day')} #{pluralize(hours_ago, 'hour')}"
+    end
+  end
 end
