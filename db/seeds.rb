2633018147e35ff306a9fcf104b6a32ede737c91
Chris Boertien <chris.boertien@gmail.com>
2014-08-17 20:11:48 -0300

    fill in more details of city in seeds and add scout reports
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
 [City, Player, Raid, Scout, LevelProgress].each &:delete_all
 
 Player.create!(name: 'Player').tap do |player|
-  player.cities.create!(name: 'City 1', level: 5, x: 0, y: 0).tap do |city|
+  player.cities.create!(name: 'City 1', level: 5, x: 0, y: 0, acropolis: 500, timber_production: 100, bronze_production: 100, food_production: 100).tap do |city|
+    city.scouts.create!(player: player, food: 1500, timber: 1500, bronze: 1500, scouted_at: Time.now - 3.days)
     city.raids.create!(player: player, food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 2.days)
   end
-  player.cities.create!(name: 'City 2', level: 10, x: 5, y: 5).tap do |city|
+  player.cities.create!(name: 'City 2', level: 10, x: 5, y: 5, acropolis: 500, timber_production: 100, bronze_production: 100, food_production: 100).tap do |city|
+    city.scouts.create!(player: player, food: 1500, timber: 1500, bronze: 1500, scouted_at: Time.now - 6.days)
     city.raids.create!(player: player, food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 8.days)
   end
 end
