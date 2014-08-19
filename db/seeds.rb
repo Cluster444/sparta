39fdccaa10bfba12854899e984201dda929d182b
Chris Boertien <chris.boertien@gmail.com>
2014-08-19 02:41:44 -0300

    generate some more cities for the seed
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 [City, Player, Raid, Scout, LevelProgress].each &:delete_all
 
 Player.create!(name: 'Player').tap do |player|
-  player.cities.create!(name: 'City 1', level: 5, x: 0, y: 0, acropolis: 500, timber_production: 100, bronze_production: 100, food_production: 100).tap do |city|
-    city.scouts.create!(player: player, food: 1500, timber: 1500, bronze: 1500, scouted_at: Time.now - 3.days)
-    city.raids.create!(player: player, food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 2.days)
-  end
-  player.cities.create!(name: 'City 2', level: 10, x: 5, y: 5, acropolis: 500, timber_production: 100, bronze_production: 100, food_production: 100).tap do |city|
-    city.scouts.create!(player: player, food: 1500, timber: 1500, bronze: 1500, scouted_at: Time.now - 6.days)
-    city.raids.create!(player: player, food: 1000, timber: 1000, bronze: 1000, capacity: 5000, raided_at: Time.now - 8.days)
+  50.times do |n|
+    player.cities.create!(name: "City #{n+1}", level: 5, x: 0, y: 0, acropolis: 5000, timber_production: 100, bronze_production: 100, food_production: 100).tap do |city|
+      5.times do |s|
+        city.scouts.create!(player: player, food: 8000, timber: 8000, bronze: 8000, reported_at: Time.now - (s+1).days)
+        city.raids.create!(player: player, food: 3000, timber: 3000, bronze: 3000, capacity: 10000, reported_at: Time.now - ((s+1).days - 1.hour))
+      end
+    end
   end
 end
