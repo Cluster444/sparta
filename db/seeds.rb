77310b8b3cf088aec9732dbe9145341dbb0fbb96
Chris Boertien <chris.boertien@gmail.com>
2014-08-19 03:08:20 -0300

    change reported at time on seeds
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ Player.create!(name: 'Player').tap do |player|
   50.times do |n|
     player.cities.create!(name: "City #{n+1}", level: 5, x: 0, y: 0, acropolis: 5000, timber_production: 100, bronze_production: 100, food_production: 100).tap do |city|
       5.times do |s|
-        city.scouts.create!(player: player, food: 8000, timber: 8000, bronze: 8000, reported_at: Time.now - (s+1).days)
-        city.raids.create!(player: player, food: 3000, timber: 3000, bronze: 3000, capacity: 10000, reported_at: Time.now - ((s+1).days - 1.hour))
+        city.scouts.create!(player: player, food: 8000, timber: 8000, bronze: 8000, reported_at: Time.now - (5.days - n.days))
+        city.raids.create!(player: player, food: 3000, timber: 3000, bronze: 3000, capacity: 10000, reported_at: Time.now - (5.days - n.days))
       end
     end
   end
