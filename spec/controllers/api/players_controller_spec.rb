ef6f38d1b35d5b9c6e5056c664c10e2a134dc49b
Chris Boertien <chris.boertien@gmail.com>
2014-08-20 05:20:23 -0300

    wrap in show describe block
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- spec/controllers/api/players_controller_spec.rb
+++ spec/controllers/api/players_controller_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe Api::PlayersController do
   render_views
 
-  it 'provides details for a player' do
-    player_attrs = {'name' => 'John Smith', 'x' => 100, 'y' => -150}
-    player = create :player, player_attrs
+  describe '#show' do
+    it 'provides details for a player' do
+      player_attrs = {'name' => 'John Smith', 'x' => 100, 'y' => -150}
+      player = create :player, player_attrs
 
-    get :show, id: player.id, format: 'json'
-    player_response = JSON.parse(response.body)
-    expect(player_response).to eq({'id' => player.id}.merge(player_attrs))
+      get :show, id: player.id, format: 'json'
+      player_response = JSON.parse(response.body)
+      expect(player_response).to eq({'id' => player.id}.merge(player_attrs))
+    end
   end
 end
