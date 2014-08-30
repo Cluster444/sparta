86c059a063b1ac9a58acea0e119d6c6147c009a7
Chris Boertien <chris.boertien@gmail.com>
2014-08-30 20:47:32 -0300

    validate the name of a player
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,6 +4,8 @@ class Player < ActiveRecord::Base
   has_many :raids, through: 'cities'
   has_many :scouts, through: 'cities'
 
+  validates :name, presence: true
+
   def coordinates
     Coordinates.new(x, y)
   end
