59e481596fa6b58a92863e24edaa66073935443c
Chris Boertien <chris.boertien@gmail.com>
2014-09-08 01:58:35 -0300

    set the current application time zone to the users time zone if it is set
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  around_action :set_time_zone, if: :current_user
+
   layout proc {|c| c.request.xhr? ? false : 'application'}
 
   rescue_from CanCan::AccessDenied do
     redirect_to new_user_session_path, alert: 'Sign in to continue'
+  end
+
+  private
+
+  def set_time_zone(&block)
+    Time.use_zone(current_user.player.try(:timezone), &block)
   end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe ApplicationController do
     def access_denied
       raise CanCan::AccessDenied
     end
+
+    def timezone
+      @time_zone = Time.zone
+      @time_now = Time.zone.now.iso8601
+      render nothing: true
+    end
   end
 
   context 'when access is denied' do
@@ -19,6 +25,48 @@ RSpec.describe ApplicationController do
       it 'flashes that the user should sign in to continue' do
         get :access_denied
         expect(flash[:alert]).to match(/sign in to continue/i)
+      end
+    end
+  end
+
+  context 'when an action is called' do
+    before { routes.draw { get "timezone" => "anonymous#timezone" } }
+
+    context 'and a player is signed in' do
+      let(:user) { create :user }
+      let(:player) { create :player, user: user }
+      before { sign_in user }
+
+      context 'and the player has set their time zone' do
+        before { player.update(timezone: 'Eastern Time (US & Canada)') }
+
+        it 'sets the time zone to the users time zone' do
+          Timecop.freeze 'Jan 14th, 2014 10:00 UTC' do
+            get :timezone
+            expect(assigns(:time_zone).name).to eq 'Eastern Time (US & Canada)'
+            expect(assigns(:time_now)).to eq '2014-01-14T05:00:00-05:00'
+          end
+        end
+      end
+
+      context 'and the player has not set their time zone' do
+        it 'sets the time zone to UTC' do
+          Timecop.freeze 'Jan 14th, 2014 10:00 UTC' do
+            get :timezone
+            expect(assigns(:time_zone).name).to eq 'UTC'
+            expect(assigns(:time_now)).to eq '2014-01-14T10:00:00Z'
+          end
+        end
+      end
+    end
+
+    context 'and not signed in' do
+      it 'sets the time zone to UTC' do
+        Timecop.freeze 'Jan 14th, 2014 10:00 UTC' do
+          get :timezone
+          expect(assigns(:time_zone).name).to eq 'UTC'
+          expect(assigns(:time_now)).to eq '2014-01-14T10:00:00Z'
+        end
       end
     end
   end
